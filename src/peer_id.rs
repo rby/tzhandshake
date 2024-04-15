904e8acb9bde9b50fc991234613937c4446d4cc3
r5 <ramzi.benyahia@gmail.com>
2024-04-15 20:56:36 +0100

    [serde] Serialize nonces to send over the wire

--- src/peer_id.rs
+++ src/peer_id.rs
@@ -1,8 +1,14 @@
 use crypto_box::{Nonce as CryptoNonce, PublicKey, SecretKey};
-use serde::{de, Deserialize};
+use serde::{de, ser, Deserialize, Serialize};
 
 #[derive(Debug)]
-struct Nonce(CryptoNonce);
+pub struct Nonce(CryptoNonce);
+
+impl From<[u8; 24]> for Nonce {
+    fn from(value: [u8; 24]) -> Self {
+        Nonce(CryptoNonce::from(value))
+    }
+}
 
 impl<'de> Deserialize<'de> for Nonce {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
@@ -12,6 +18,15 @@ impl<'de> Deserialize<'de> for Nonce {
         let mut bytes = [0u8; 24];
         serdect::array::deserialize_hex_or_bin(&mut bytes, deserializer)?;
         Ok(Nonce(CryptoNonce::from(bytes)))
+    }
+}
+
+impl Serialize for Nonce {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: ser::Serializer,
+    {
+        serdect::array::serialize_hex_upper_or_bin(&self.0, serializer)
     }
 }
 
@@ -42,5 +57,12 @@ mod test {
         });
         let res = PeerId::deserialize(content);
         assert!(res.is_ok())
+    }
+    #[test]
+    fn it_serializes_nonces() -> Result<(), serde_json::Error> {
+        let nonce = Nonce::from([5; 24]);
+        let s = serde_json::to_string(&nonce)?;
+        assert_eq!(s, "\"050505050505050505050505050505050505050505050505\"");
+        Ok(())
     }
 }
